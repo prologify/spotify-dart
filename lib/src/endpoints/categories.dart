9ec4812afa57817d8696006593cc9c115c44af57
mogilevsky <mogilevsky.ivan@gmail.com>
2019-06-03 17:52:08 +0300

    add method for category playlists

--- lib/src/endpoints/categories.dart
+++ lib/src/endpoints/categories.dart
@@ -53,4 +53,27 @@ class Categories extends EndpointPaging {
 
     return Category.fromJson(map);
   }
+
+  /// [country] - a country: an ISO 3166-1 alpha-2 country code. Provide this
+  /// parameter to ensure that the category exists for a particular country.
+  ///
+  /// [locale] - the desired language, consisting of an ISO 639-1 language code
+  /// and an ISO 3166-1 alpha-2 country code, joined by an underscore. For
+  /// example: es_MX, meaning "Spanish (Mexico)". Provide this parameter if you
+  /// want the category strings returned in a particular language. Note that,
+  /// if locale is not supplied, or if the specified language is not available,
+  /// the category strings returned will be in the Spotify default language
+  /// (American English).
+  ///
+  /// [categoryId] - the Spotify category ID for the category.
+  Pages<PlaylistSimple> getCategoryPlaylists(String categoryId,
+      {String country}) {
+    final String query = _buildQuery({'country': country});
+
+    return _getPages(
+        '$_path/$categoryId/playlists?$query',
+        (json) => PlaylistSimple.fromJson(json),
+        'playlists',
+        (json) => PlaylistSimple.fromJson(json));
+  }
 }
